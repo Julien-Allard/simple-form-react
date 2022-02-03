5ca4f475629bc722f3c43089fe61dd73a3057553
Julien-Allard <julalldev@gmail.com>
2022-02-03 16:21:42 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { library } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+library.add(faEye);
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [checkPass, setCheckPass] = useState("");
+  const [results, setResults] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event);
+    if (checkPass !== pass) {
+      alert("Vos deux mots de passe ne sont pas identiques !");
+    } else {
+      setResults(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        {!results ? (
+          <>
+            <h1>Create account</h1>
+            <form onSubmit={handleSubmit}>
+              <span>Name</span>
+              <input
+                value={name}
+                type="text"
+                placeholder="Bob Martin"
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+              <span>Email</span>
+              <input
+                value={email}
+                type="text"
+                placeholder="bob@mail.com"
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+              <span>Password</span>
+              <input
+                className={pass !== checkPass ? "red-border" : null}
+                value={pass}
+                type="password"
+                placeholder="password"
+                onChange={(event) => {
+                  setPass(event.target.value);
+                }}
+              />
+              <FontAwesomeIcon icon="eye" />
+              <span>Confirm your password</span>
+              <input
+                className={pass !== checkPass ? "red-border" : null}
+                value={checkPass}
+                type="password"
+                placeholder="password"
+                onChange={(event) => {
+                  setCheckPass(event.target.value);
+                }}
+              />
+              <input className="register-btn" value="Register" type="submit" />
+            </form>
+          </>
+        ) : null}
+
+        {results ? (
+          <>
+            <h1>Results</h1>
+            <div className="results">
+              <div className="user-info">
+                <p>Name : {name}</p>
+                <p>Email : {email}</p>
+                <p>Password : {pass}</p>
+              </div>
+              <button
+                className="edit-btn"
+                onClick={() => {
+                  setResults(false);
+                }}
+              >
+                Edit your informations
+              </button>
+            </div>
+          </>
+        ) : null}
+      </div>
     </div>
   );
 }
